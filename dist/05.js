56687bb57b79c56e34866d5b76f525d4e53ba39d
Vitor Giovane <vitorgpalves@gmail.com>
2019-07-26 14:22:28 -0300

    feature: finishes the class `Tipo object e any`
    ===
    05

---
+++ dist/05.js
@@ -0,0 +1,16 @@
+let obj1 = {
+    firstName: "Vitor",
+    lastName: "Giovane"
+};
+// obj = "Text"
+let obj2 = {
+    category: "First category",
+    product: "First product"
+};
+obj2 = 2;
+let variable = 4;
+variable = {};
+variable = "string";
+variable = false;
+let myArray = [2, "string", false, { name: "Vitor Giovane" }];
+let myArray2 = [2, "string", false, { name: "Vitor Giovane" }];
---
+++ src/05.ts
@@ -0,0 +1,21 @@
+let obj1: object = {
+  firstName: "Vitor",
+  lastName: "Giovane"
+}
+
+// obj = "Text"
+
+let obj2: Object = {
+  category: "First category",
+  product: "First product"
+}
+
+obj2 = 2
+
+let variable: any = 4
+variable = {}
+variable = "string"
+variable = false
+
+let myArray: Array<any> = [2, "string", false, { name: "Vitor Giovane" }]
+let myArray2: any[] = [2, "string", false, { name: "Vitor Giovane" }]
