2abfcf13725145abe182ac9514ce0b971a4e98e5
Vitor Giovane <vitorgpalves@gmail.com>
2019-07-26 11:59:06 -0300

    feature: finishes the class `Variáveis var e let`
    ==
    02

---
+++ dist/02.js
@@ -0,0 +1,32 @@
+function showWithVar() {
+    var text = "Vitor Giovane";
+    if (true) {
+        var text = "text";
+    }
+    console.log(text);
+}
+function showWithLet() {
+    let text = "Vitor Giovane";
+    if (true) {
+        let text = "text";
+    }
+    console.log(text);
+}
+const showWithVarArrowFunction = () => {
+    var text = "Vitor Giovane";
+    if (true) {
+        var text = "text";
+    }
+    console.log(text);
+};
+const showWithLetArrowFunction = () => {
+    let text = "Vitor Giovane";
+    if (true) {
+        let text = "text";
+    }
+    console.log(text);
+};
+showWithVar();
+showWithLet();
+showWithVarArrowFunction();
+showWithLetArrowFunction();
